2fa7b928afe940f62d9b3074ee4c2b1b4dbc13f3
Christopher Pulte <cp526@cam.ac.uk>
2023-03-15 17:08:45 +0000

    wib

--- memory.h
+++ memory.h
@@ -81,14 +81,14 @@ static inline int hyp_page_count(struct hyp_pool *pool, void *addr)
 /*@ ensures H2.pool == {H.pool}@start @*/
 /*@ ensures return == ((H2.vmemmap)[phys / 4096]).refcount @*/
 {
-        /*CN*/unpack Hyp_pool(pool, hyp_vmemmap, hyp_physvirt_offset);
+        /*CN*//*@ unpack Hyp_pool(pool, hyp_vmemmap, hyp_physvirt_offset); @*/
 	struct hyp_page *p = hyp_virt_to_page(addr);
-        /*CN*/instantiate good, hyp_page_to_pfn(p);
-        /*CN*/instantiate vmemmap_wf, hyp_page_to_pfn(p);
+        /*CN*//*@instantiate good<struct hyp_page>, cn_hyp_page_to_pfn(__hyp_vmemmap,p);@*/
+        /*CN*//*@instantiate vmemmap_wf, cn_hyp_page_to_pfn(__hyp_vmemmap, p);@*/
 	/* TODO originally: return p->refcount.  Introducing 'ret' here, so we can pack resources before returning; */
 	int ret = p->refcount;
 
-        /*CN*/pack Hyp_pool(pool, hyp_vmemmap, hyp_physvirt_offset);
+        /*CN*//*@pack Hyp_pool(pool, hyp_vmemmap, hyp_physvirt_offset);@*/
         return ret;
 }
 
