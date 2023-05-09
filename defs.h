cc595c25592652c374637565d218ee8db88eeec1
Christopher Pulte <cp526@cam.ac.uk>
2023-05-09 12:06:58 +0100

    adjust for new function definition syntax

--- defs.h
+++ defs.h
@@ -4,52 +4,52 @@ function (integer) order_align (integer x, integer sz)
 function (boolean) page_aligned (integer x, integer sz)
 function (integer) page_size_of_order (integer sz)
 
-function (integer) page_size () { return 4096; }
-function (integer) max_order () { return 11; }
-function (integer) hyp_no_order () { return 255; }
-function (integer) sizeof_struct_hyp_page(){ return sizeof <struct hyp_page>; }
-function (integer) sizeof_struct_list_head() { return sizeof <struct list_head>; }
+function (integer) page_size () { 4096 }
+function (integer) max_order () { 11 }
+function (integer) hyp_no_order () { 255 }
+function (integer) sizeof_struct_hyp_page(){ sizeof <struct hyp_page> }
+function (integer) sizeof_struct_list_head() { sizeof <struct list_head> }
 
 function (integer) cn_hyp_page_to_pfn(integer hypvmemmap, pointer p) {
-  return (((integer) p) - hypvmemmap) / sizeof<struct hyp_page>;
+  (((integer) p) - hypvmemmap) / sizeof<struct hyp_page>
 }
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (integer) cn__hyp_pa(integer physvirtoffset, pointer virt) {
-  return (((integer)(virt)) + physvirtoffset);
+  (((integer)(virt)) + physvirtoffset)
 }
 
 
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (integer) cn_hyp_phys_to_pfn(integer phys) {
-  return phys / 4096;
+  phys / 4096
 }
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (integer) cn_hyp_virt_to_pfn (integer physvirtoffset, pointer virt) {
-  return cn_hyp_phys_to_pfn(cn__hyp_pa(physvirtoffset, virt));
+  cn_hyp_phys_to_pfn(cn__hyp_pa(physvirtoffset, virt))
 }
 
 
 function (integer) cn_hyp_pfn_to_phys(integer pfn) {
-  return pfn*4096;
+  pfn*4096
 }
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (integer) cn_hyp_page_to_phys(integer hypvmemmap, pointer page) {
-  return cn_hyp_pfn_to_phys((cn_hyp_page_to_pfn(hypvmemmap, page)));
+  cn_hyp_pfn_to_phys((cn_hyp_page_to_pfn(hypvmemmap, page)))
 }
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (pointer) cn__hyp_va(integer physvirtoffset, integer phys) {
-  return ((pointer) (phys - physvirtoffset));
+  ((pointer) (phys - physvirtoffset))
 }
 
 /* copied and adjusted from the corresponding macro definition in memory.h */
 function (pointer) cn_hyp_page_to_virt(integer physvirtoffset, 
                                        integer hypvmemmap, pointer page) {
-  return cn__hyp_va(physvirtoffset, cn_hyp_page_to_phys(hypvmemmap, page));
+  cn__hyp_va(physvirtoffset, cn_hyp_page_to_phys(hypvmemmap, page))
 }
 
 
@@ -59,26 +59,25 @@ datatype excludes {
 
 function (boolean) excluded (datatype excludes ex, integer i)
 {
-  return (
-    (ex.any && (
+  ex.any && (
       (ex.do_ex1 && (i == ex.ex1))
       || (ex.do_ex2 && (i == ex.ex2))
-  )));
+  )
 }
 
 function (datatype excludes) exclude_none ()
 {
-  return Exclude {any: 0 < 0, do_ex1: 0 < 0, ex1: 0, do_ex2: 0 < 0, ex2: 0};
+  Exclude {any: 0 < 0, do_ex1: 0 < 0, ex1: 0, do_ex2: 0 < 0, ex2: 0}
 }
 
 function (datatype excludes) exclude_one (integer ex1)
 {
-  return Exclude {any: 0 < 1, do_ex1: 0 < 1, ex1: ex1, do_ex2: 0 < 0, ex2: 0};
+  Exclude {any: 0 < 1, do_ex1: 0 < 1, ex1: ex1, do_ex2: 0 < 0, ex2: 0}
 }
 
 function (datatype excludes) exclude_two (integer ex1, integer ex2)
 {
-  return Exclude {any: 0 < 1, do_ex1: 0 < 1, ex1: ex1, do_ex2: 0 < 1, ex2: ex2};
+  Exclude {any: 0 < 1, do_ex1: 0 < 1, ex1: ex1, do_ex2: 0 < 1, ex2: ex2}
 }
 
 
@@ -95,14 +94,12 @@ function (boolean) vmemmap_good_pointer (integer physvirt_offset, pointer p,
   let offs = ((integer) p) + physvirt_offset;
   // let idx = offs / hp_sz;
   let idx = offs / (page_size ());
-  return (
     (((offs / (page_size ())) * (page_size ())) == offs)
     && (start_i <= idx)
     && (idx < end_i)
     && (vmemmap[idx].refcount == 0)
     && (vmemmap[idx].order != (hyp_no_order ()))
     && (not (excluded (ex, idx)))
-  );
 }
 
 
@@ -111,13 +108,13 @@ function (boolean) page_group_ok (integer page_index,
 {
   let page = vmemmap[page_index];
   let start_i = (pool.range_start) / (page_size ());
-  return ((page.order == (hyp_no_order ()))
+  ((page.order == (hyp_no_order ()))
     || (each (i: 1, 10; (not (
         ((order_align(page_index, i)) < page_index)
         && (start_i <= (order_align(page_index, i)))
         && (i <= ((vmemmap[(order_align(page_index, i))]).order))
         && (((vmemmap[(order_align(page_index, i))]).order) != (hyp_no_order ()))
-    )))));
+    )))))
 }
 
 // There are no `AllocatorPage`s outputs to pass as arguments in `hyp_pool_init`
@@ -131,7 +128,6 @@ function (boolean) init_vmemmap_page (integer page_index,
   //   ((integer)vmemmap_pointer) + (hp_sz * page_index)));
   let page = vmemmap[page_index];
   // let self_node_pointer = (pointer)(((integer)page_pointer) + (offsetof (hyp_page, node)));
-  return (
     (page.order == 0)
     && (page.refcount == 1)
     // && (page.pool == pool_pointer)
@@ -139,15 +135,12 @@ function (boolean) init_vmemmap_page (integer page_index,
     // && (page.node.prev == self_node_pointer)
     && (order_aligned(page_index, 0))
     && (((page_index * (page_size ())) + page_size_of_order(page.order)) <= pool.range_end)
-  );
 }
 
 function (boolean) vmemmap_normal_order_wf (integer page_index, struct hyp_page page, struct hyp_pool pool) {
-  return (
     (0 <= page.order && ((page.order < pool.max_order) && (page.order < (max_order()))))
     && order_aligned(page_index, page.order)
     && (((page_index * (page_size ())) + page_size_of_order(page.order)) <= pool.range_end)
-  );
 }
 
 
@@ -160,7 +153,6 @@ function (boolean) vmemmap_wf (integer page_index,
   let page = vmemmap[page_index];
   // let prev = page.node.prev;
   // let next = page.node.next;
-  return (
     /* representable as an unsigned short */
     (0 <= page.refcount) && (page.refcount < (power(2, 16)))
     // && (page.pool == pool_pointer)
@@ -169,7 +161,6 @@ function (boolean) vmemmap_wf (integer page_index,
     // && ((next == self_node_pointer)
     //     || ((page.refcount == 0) && ((page.order != (hyp_no_order ())))))
     && (page_group_ok(page_index, vmemmap, pool))
-  );
 }
 
 function (boolean) vmemmap_l_wf (integer page_index, integer physvirt_offset,
@@ -216,7 +207,7 @@ function (boolean) vmemmap_l_wf (integer page_index, integer physvirt_offset,
   /* there is no self-loop case for this node type, as it is cleared unless it is
      present in the per-order free list */
   let nonempty_clause = (prev != self_node_pointer) && (next != self_node_pointer);
-  return (prev_clause && next_clause);
+  (prev_clause && next_clause)
 }
 
 /*
@@ -265,7 +256,6 @@ function (boolean) freeArea_cell_wf (integer cell_index, integer physvirt_offset
   let next_page_index = (((integer) next_page_pointer) + physvirt_offset) / (page_size ());
   let next_page = vmemmap[next_page_index];
   let off_i = physvirt_offset / (page_size());
-  return (
     ((prev == cell_pointer) == (next == cell_pointer))
     && ((prev == cell_pointer) || (
         (vmemmap_good_pointer (physvirt_offset, prev_page_pointer, vmemmap, pool.range_start, pool.range_end, ex))
@@ -279,7 +269,6 @@ function (boolean) freeArea_cell_wf (integer cell_index, integer physvirt_offset
         // && (next_page.node.prev == cell_pointer)
         && ((APs[next_page_index - off_i]).prev == cell_pointer)
     ))
-  );
 }
 
 function (boolean) hyp_pool_wf (pointer pool_pointer, struct hyp_pool pool,
@@ -297,7 +286,6 @@ function (boolean) hyp_pool_wf (pointer pool_pointer, struct hyp_pool pool,
     ((integer)vmemmap_pointer) + (hp_sz * end_i)));
   let range_start_virt = range_start - physvirt_offset;
   let range_end_virt = range_end - physvirt_offset;
-  return (
     (0 <= range_start)
     && (range_start < range_end)
     && (range_end < (power(2, 52)))
@@ -315,13 +303,12 @@ function (boolean) hyp_pool_wf (pointer pool_pointer, struct hyp_pool pool,
         || (((integer) vmemmap_boundary_pointer) <= ((integer) pool_pointer)))
     && (((((integer) pool_pointer) + pool_sz) <= range_start_virt)
         || (range_end_virt <= ((integer) pool_pointer)))
-  );
 }
 
 function (integer) get_order_uf (integer size)
 
 function (pointer) virt (pointer phys, integer physvirt_offset) {
-  return (pointer) (((integer) phys) - physvirt_offset);
+  (pointer) (((integer) phys) - physvirt_offset)
 }
 
 
