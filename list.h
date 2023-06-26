da979c0f897a147bca29b58536512e6a0b07ae5b
Christopher Pulte <cp526@cam.ac.uk>
2023-06-26 23:22:01 +0100

    remove last uses of `when`

--- list.h
+++ list.h
@@ -36,13 +36,13 @@ static inline bool __list_del_entry_valid(struct list_head *entry)
 
 static inline void __list_del(struct list_head * prev, struct list_head * next)
 /*@ requires take O1 = Owned(prev) @*/
-/*@ requires take O2 = Owned(next) when (prev != next) @*/
+/*@ requires take O2 = O_struct_list_head(next, prev != next) @*/
 /*@ ensures take O1R = Owned(prev) @*/
-/*@ ensures take O2R = Owned(next) when (prev != next) @*/
-/*@ ensures (prev == next) || {(*next).next} unchanged @*/
+/*@ ensures take O2R = O_struct_list_head(next, prev != next) @*/
+/*@ ensures (prev == next) || (O2.next == O2R.next) @*/
 /*@ ensures (prev == next) || {(*prev).prev} unchanged @*/
 /*@ ensures (*prev).next == next @*/
-/*@ ensures (prev == next) || ((*next).prev == prev) @*/
+/*@ ensures (prev == next) || (O2R.prev == prev) @*/
 /*@ ensures (prev != next) || ((*prev).prev == prev) @*/
 {
         /*CN*/ if(prev != next);
@@ -54,18 +54,18 @@ static inline void __list_del(struct list_head * prev, struct list_head * next)
 static inline void __list_del_entry(struct list_head *entry)
 /*@ requires take O1 = Owned(entry) @*/
 /*@ requires let prev = (*entry).prev; let next = (*entry).next @*/
-/*@ requires take O2 = Owned(prev) when (prev != entry) @*/
-/*@ requires take O3 = Owned(next) when (prev != next) @*/
+/*@ requires take O2 = O_struct_list_head(prev, prev != entry) @*/
+/*@ requires take O3 = O_struct_list_head(next, prev != next) @*/
 /*@ requires (prev != entry) || (next == entry) @*/
 /*@ ensures take O1R = Owned(entry) @*/
 /*@ ensures {*entry} unchanged @*/
-/*@ ensures take O2R = Owned(prev) when (prev != entry) @*/
-/*@ ensures take O3R = Owned(next) when (prev != next) @*/
-/*@ ensures (prev == next) || {(*next).next} unchanged @*/
-/*@ ensures (prev == next) || {(*prev).prev} unchanged @*/
-/*@ ensures (prev == entry) || ((*prev).next == next) @*/
-/*@ ensures (prev == next) || ((*next).prev == prev) @*/
-/*@ ensures (prev != next) || ((prev == entry) || ((*prev).prev == prev)) @*/
+/*@ ensures take O2R = O_struct_list_head(prev, prev != entry) @*/
+/*@ ensures take O3R = O_struct_list_head(next, prev != next) @*/
+/*@ ensures (prev == next) || (O3.next == O3R.next) @*/
+/*@ ensures (prev == next) || (O2.prev == O2R.prev) @*/
+/*@ ensures (prev == entry) || (O2R.next == next) @*/
+/*@ ensures (prev == next) || (O3R.prev == prev) @*/
+/*@ ensures (prev != next) || ((prev == entry) || (O2R.prev == prev)) @*/
 {
         /*CN*/ if(entry->prev != entry);
         /*CN*/ if(entry->prev != entry->next);
@@ -79,16 +79,16 @@ static inline void list_del_init(struct list_head *entry)
 /*@ requires take O1 = Owned(entry) @*/
 /*@ requires let prev = (*entry).prev; let next = (*entry).next @*/
 /*@ requires take O2 = Owned(prev) @*/
-/*@ requires take O3 = Owned(next) when (prev != next) @*/
+/*@ requires take O3 = O_struct_list_head(next, prev != next) @*/
 /*@ requires (*entry).prev != entry @*/
 /*@ ensures take O1R = Owned(entry) @*/
 /*@ ensures (*entry).prev == entry; (*entry).next == entry @*/
 /*@ ensures take O2R = Owned(prev) @*/
-/*@ ensures take O3R = Owned(next) when (prev != next) @*/
-/*@ ensures (prev == next) || {(*next).next} unchanged @*/
+/*@ ensures take O3R = O_struct_list_head(next, prev != next) @*/
+/*@ ensures (prev == next) || (O3.next == O3R.next) @*/
 /*@ ensures (prev == next) || {(*prev).prev} unchanged @*/
 /*@ ensures (*prev).next == next @*/
-/*@ ensures (prev == next) || ((*next).prev == prev) @*/
+/*@ ensures (prev == next) || (O3R.prev == prev) @*/
 /*@ ensures (prev != next) || ((*prev).prev == prev) @*/
 {
         /*CN*/ if(entry->prev != entry);
@@ -109,12 +109,12 @@ static inline bool __list_add_valid(struct list_head *new,
 static inline void __list_add(struct list_head *new,
 			      struct list_head *prev,
 			      struct list_head *next)
-/*@ requires take O1 = Owned(new); take O2 = Owned(prev); take O3 = Owned(next) when (prev != next) @*/
-/*@ ensures take O1R = Owned(new); take O2R = Owned(prev); take O3R = Owned(next) when (prev != next) @*/
+/*@ requires take O1 = Owned(new); take O2 = Owned(prev); take O3 = O_struct_list_head(next, prev != next) @*/
+/*@ ensures take O1R = Owned(new); take O2R = Owned(prev); take O3R = O_struct_list_head(next, prev != next) @*/
 /*@ ensures (prev == next) || {(*prev).prev} unchanged @*/
-/*@ ensures (prev == next) || {(*next).next} unchanged @*/
+/*@ ensures (prev == next) || (O3.next == O3R.next) @*/
 /*@ ensures (*prev).next == new @*/
-/*@ ensures (prev == next) || ((*next).prev == new) @*/
+/*@ ensures (prev == next) || (O3R.prev == new) @*/
 /*@ ensures (prev != next) || ((*prev).prev == new) @*/
 /*@ ensures (*new).next == next; (*new).prev == prev @*/
 {
@@ -135,12 +135,12 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head)
 /*@ requires take O1 = Owned(new) @*/
 /*@ requires take O2 = Owned(head) @*/
 /*@ requires let prev = (*head).prev; let next = head @*/
-/*@ requires take O3 = Owned(prev) when (prev != next) @*/
-/*@ ensures take O1R = Owned(new); take O2R = Owned(head); take O3R = Owned(prev) when (prev != next) @*/
-/*@ ensures (prev == next) || {(*prev).prev} unchanged @*/
+/*@ requires take O3 = O_struct_list_head(prev, prev != next) @*/
+/*@ ensures take O1R = Owned(new); take O2R = Owned(head); take O3R = O_struct_list_head(prev, prev != next) @*/
+/*@ ensures (prev == next) || (O3.prev == O3R.prev) @*/
 /*@ ensures (prev == next) || {(*head).next} unchanged @*/
 /*@ ensures (*head).prev == new @*/
-/*@ ensures (prev == next) || ((*prev).next == new) @*/
+/*@ ensures (prev == next) || (O3R.next == new) @*/
 /*@ ensures (prev != next) || ((*head).next == new) @*/
 /*@ ensures (*new).next == next; (*new).prev == prev @*/
 {
