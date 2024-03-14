56af5d410dc3e8ad5e8a329014dd2b9dab96770b
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2024-03-14 17:59:47 +0000

    Testing: avoid signed ptrdiff in hyp_page_to_pfn
    Converting this macro into an inline function permits me to give a
    specification where the division is done only at an unsigned type. Maybe it
    will give measurably different SMT performance?

--- memory.h
+++ memory.h
@@ -57,10 +57,22 @@ static inline phys_addr_t hyp_virt_to_phys(void *addr)
 #define hyp_virt_to_page(virt)	hyp_phys_to_page(__hyp_pa(virt))
 #define hyp_virt_to_pfn(virt)	hyp_phys_to_pfn(__hyp_pa(virt))
 
-#define hyp_page_to_pfn(page)	((struct hyp_page *)(page) - hyp_vmemmap)
+#define hyp_page_to_pfn_macro(page)	((struct hyp_page *)(page) - hyp_vmemmap)
 #define hyp_page_to_phys(page)  hyp_pfn_to_phys((hyp_page_to_pfn(page)))
 #define hyp_page_to_virt(page)	__hyp_va(hyp_page_to_phys(page))
 #define hyp_page_to_pool(page)	(((struct hyp_page *)page)->pool)
+
+static inline u64 hyp_page_to_pfn(struct hyp_page *page)
+/*@ accesses __hyp_vmemmap @*/
+/*@ requires let max_pfn = shift_left(2u64, 64u64 - page_size()) - 1u64 @*/
+/*@ requires let offs = ((u64) page) - ((u64) __hyp_vmemmap) @*/
+/*@ requires offs <= max_pfn * (sizeof<struct hyp_page>) @*/
+/*@ requires mod(offs, sizeof<struct hyp_page>) == 0u64 @*/
+/*@ ensures return == offs / (sizeof<struct hyp_page>) @*/
+/*@ ensures {__hyp_vmemmap} unchanged @*/
+{
+  return hyp_page_to_pfn_macro(page);
+}
 
 /* static inline int hyp_page_count(void *addr) */
 /* { */
