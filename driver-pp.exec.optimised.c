03442adaf59fb7ae76e39c660cb79bb52c4bfc2f
Hiroyuki Katsura <hk590@cam.ac.uk>
2026-03-20 16:24:55 +0000

    put constants outside of the loop

--- driver-pp.exec.optimised.c
+++ driver-pp.exec.optimised.c
@@ -7094,30 +7094,28 @@ static void AllocatorPageZeroPart(cn_pointer* zero_start, cn_bits_u8* order, enu
   cn_bits_u64* length;
   length = cn_bits_u64_sub(region_length, convert_to_cn_bits_u64(sizeof(struct list_head)));
   update_cn_error_message_info("  take Bytes = each (u64 i; (start <= i) && (i < (start + length)))\n       ^../../cn-pKVM-buddy-allocator-case-study/driver-pp.c:725:8:");
-
+  // ownership
   owned_char_range(zero_start, length, spec_mode, loop_ownership);
   cn_map* V_cn = map_create();
   {
-      unsigned long long i = start->val;
-      unsigned long long end = start->val + length->val;
-      while (i < end) {
-	 char *ptr_data = (char*)i;
-	 cn_map_set(V_cn, convert_to_cn_bits_i64(i), convert_to_cn_bits_u8(*ptr_data));
-	 i++;
+    cn_bits_u64* i = cast_cn_bits_u64_to_cn_bits_u64(start);
+    cn_bits_u64 *start_cn = cast_cn_bits_u64_to_cn_bits_u64(start);
+    cn_bits_u64 *end_cn = cn_bits_u64_add(start, length);
+    while (convert_from_cn_bool(cn_bits_u64_lt(i, end_cn))) {
+        cn_pointer* a_14586 = cn_array_shift(convert_to_cn_pointer(0), sizeof(char), i);
+        cn_map_set(V_cn, i, convert_to_cn_bits_u8(*(char*)a_14586->ptr));
+        i++;
       }
 
   }
 
   {
     cn_bits_u64* i = cast_cn_bits_u64_to_cn_bits_u64(start);
-
-      unsigned long long i_aux = start->val;
-      unsigned long long end_aux = start->val + length->val;
-
-     while (i_aux < end_aux) {
+    cn_bits_u64 *start_cn = cast_cn_bits_u64_to_cn_bits_u64(start);
+    cn_bits_u64 *end_cn = cn_bits_u64_add(start, length);
+    while (convert_from_cn_bool(cn_bits_u64_lt(i, end_cn))) {
         cn_assert(cn_bits_u8_equality((cn_bits_u8*) cn_map_get_cn_bits_u8(V_cn, cast_cn_bits_u64_to_cn_integer(i)), convert_to_cn_bits_u8(0UL)), spec_mode);
         cn_bits_u64_increment(i);
-	i_aux++;
       }
   }
   cn_pop_msg_info();
